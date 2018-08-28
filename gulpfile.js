ff1abfc8b1a3ba3ee1e93cf0b4c3962185b98a8e
John Haiducek <jhaiduce@picard.local>
2018-08-28 17:52:08 -0400

    Generate THEMIS orbit plot as part of prepare task

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,11 @@ const replace = require('gulp-replace');
 const zip = require('gulp-zip');
 const pages = require('gh-pages');
 const sync = require('browser-sync').create();
+const cp=require('child_process')
+const changed = require('gulp-changed');
+const through = require('through2');
+const vinylFile = require('vinyl-file')
+var readline = require('readline');
 
 gulp.task('prepare', () => {
 
@@ -58,13 +63,23 @@ gulp.task('prepare', () => {
             path.dirname = 'shower/themes/ribbon/' + path.dirname;
         }));
 
+    const pictures = gulp.src([
+	'pictures/**'
+    ])
+	  /*.pipe(
+	changed('prepared/pictures')
+    )*/.pipe(makePictures()
+    ).pipe(rename( (path) => {
+	path.dirname = 'pictures' + path.dirname
+    }));
+
     const themes = merge(material, ribbon)
         .pipe(replace(
             /(<script src=")(\/shower-core\/)(shower.min.js"><\/script>)/,
             '$1../../$3', { skipBinary: true }
         ));
 
-    return merge(shower, core, themes)
+    return merge(shower, core, themes, pictures)
         .pipe(gulp.dest('prepared'));
 
 });
@@ -108,3 +123,47 @@ gulp.task('serve', () => {
         sync.reload();
     });
 });
+
+var makePictures = function () {
+    return through.obj(function (vinylFileIn, encoding, callback) {
+
+	// 1. clone new vinyl file for manipulation
+	// (See https://github.com/wearefractal/vinyl for vinyl attributes and functions)
+	var transformedFile = vinylFileIn.clone();
+
+	//console.log(vinylFile.basename)
+
+	// 2. set new contents
+	// * contents can only be a Buffer, Stream, or null
+	// * This allows us to modify the vinyl file in memory and prevents the need to write back to the file system.
+
+	if(/^plot_(.+)\.py$/.test(vinylFileIn.basename))
+	{
+	    console.log(vinylFileIn.basename)
+	    scriptname=String(vinylFileIn.basename)
+	    var cwd=String(vinylFileIn.dirname)
+	    console.log(cwd)
+	    var process=cp.spawn('python',[scriptname],options={'cwd':cwd})
+	    
+	    var lineReader = readline.createInterface(process.stdout, process.stdin);
+	    lineReader.on('line', function(line) {
+		console.log('Line: ' + line);
+	    });
+
+	    process.on('close', function(code,signal){
+		newfilename=vinylFileIn.basename.replace(/^plot_(.+).py$/,'$1.svg');
+		console.log(newfilename)
+		transformedFile = vinylFile.readSync(newfilename,
+						     options={'cwd':cwd});
+		// 3. pass along transformed file for use in next `pipe()`
+		callback(null, transformedFile);
+	    })
+	}
+	else
+	{
+	    // 3. pass along transformed file for use in next `pipe()`
+	    callback(null, transformedFile);
+	}
+
+    });
+}
